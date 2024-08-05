3a8a4231bde889ac226a27cfc90e63f7507b9dd4
= <=>
2024-08-04 23:03:42 -0400

    Created thread for rendering

--- src/bvec.rs
+++ src/bvec.rs
@@ -1,19 +1,40 @@
+use minifb::{Key, Window, WindowOptions};
+
 use std::error::Error;
 use std::fmt::Debug;
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 
 use crate::buffer::BVECBuffer;
 
 #[derive(Debug)]
 pub struct BVEC {
-    buffer: BVECBuffer,
+    //buffer: Arc<BVECBuffer>,
+    running: Arc<AtomicBool>,
+    render_handle: Option<std::thread::JoinHandle<()>>,
 }
 
 impl BVEC {
-    pub fn init() {
-        todo!();
+    pub fn new() -> BVEC {
+        Self{
+            running: Arc::new(AtomicBool::new(true)),
+            render_handle: None,
+        }
     }
 
-    pub fn end() {
-        todo!();
+    pub fn run(&mut self) {
+        let running = self.running.clone();
+
+        self.render_handle = Some(std::thread::spawn(move || {
+            while running.load(Ordering::Relaxed) {
+                println!("Joe");
+            }
+        }));
+    }
+
+    pub fn end(&mut self) {
+        self.running.store(false, Ordering::Relaxed);
+        self.render_handle.take().unwrap().join().unwrap();
+        println!("bama");
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,21 @@
 mod bvec;
 mod buffer;
+
+use bvec::BVEC;
+use buffer::BVECBuffer;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut instance = BVEC::new();
+        println!("Zero!");
+        instance.run();
+        println!("Uno!");
+        std::thread::sleep(std::time::Duration::from_micros(1000));
+        instance.end();
+        println!("Dos!");
+    }
+}
